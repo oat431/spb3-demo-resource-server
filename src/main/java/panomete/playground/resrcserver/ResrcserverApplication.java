5a3c95b4ec9cfa5c5bbcfc718457bde756c98e55
oat431 <oat431@gmail.com>
2023-10-03 03:14:49 +0700

    🛠 add: example resource server

---
+++ src/main/java/panomete/playground/resrcserver/ResrcserverApplication.java
@@ -0,0 +1,13 @@
+package panomete.playground.resrcserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResrcserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResrcserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/panomete/playground/resrcserver/ResrcserverApplicationTests.java
@@ -0,0 +1,13 @@
+package panomete.playground.resrcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResrcserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
